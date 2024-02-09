6b385711ccca03b57ed75ee9059a7918bc922ce9
saidtalfana <saidtalfana1@gmail.com>
2024-02-09 15:23:09 +0100

    third commit

--- src/Main.java
+++ src/Main.java
@@ -26,4 +26,29 @@ class Book {
     }
 
 }
+class Library{
+    List<Book> ListeBooks = new ArrayList<>();
+    List<Student> ListeStudents = new ArrayList<>();
 
+
+    public void addBook(Book book) {
+        ListeBooks.add(book);
+    }
+
+    public void removeBook(String ISBN) {
+        for (Book book : ListeBooks) {
+            if (book.ISBN.equals(ISBN)) {
+                ListeBooks.remove(book);
+                break;
+            }
+        }
+    }
+
+    public void displayBooks() {
+        for (Book book : ListeBooks) {
+            System.out.println("Title: " + book.title);
+            System.out.println("Author: " + book.author);
+            System.out.println("ISBN: " + book.ISBN);
+            System.out.println();
+        }
+    }
