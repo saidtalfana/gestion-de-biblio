b9ea8b20bf4af5f3185f11354cb12f47012f4124
saidtalfana <saidtalfana1@gmail.com>
2024-02-09 14:44:27 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Student {
+    String name;
+    String studentId;
+    String address;
+
+    public Student(String name, String studentId, String address) {
+        this.name = name;
+        this.studentId = studentId;
+        this.address = address;
+    }
+}
+
